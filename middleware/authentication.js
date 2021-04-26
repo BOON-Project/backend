ad69432fe0611e8d5b3e7ad799efead43d3135c9
Cindy <Punk.c@hotmail.com>
2021-04-26 13:58:38 +0200

    add authentication , missing validation

---
+++ middleware/authentication.js
@@ -0,0 +1,23 @@
+const customError = require("../helpers/customError");
+const User = require('../models/User');
+
+exports.authentication = async(req, res, next)=>{
+    try {
+        
+        //gonna use cookies in future!
+        const token = req.cookies.token;
+
+        //then we validate cookie. next search user with that COOKIE
+        const user = await User.findByToken(token);
+
+        //if token is not valid, then an error shows up
+        if(!user) next(customError("Cannot validate"));
+
+        //if token is valid, we're ready to go =>
+        req.user = user;
+        next();
+
+    } catch (error) {
+        next(error);
+    }
+}
