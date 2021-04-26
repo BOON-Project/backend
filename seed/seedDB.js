2eacd5b128e7fbcf6ca81383b4f822bcccdb16bb
INes <inesfcnogueira@gmail.com>
2021-04-26 15:41:38 +0200

    added seed

---
+++ seed/seedDB.js
@@ -0,0 +1,128 @@
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const Skill = require("../models/Skill");
+const Task = require("../models/Task");
+const faker = require("faker");
+const env = require("../config/config");
+
+mongoose.connect(env.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+})
+.then(() => console.log('Connection to db established'))
+.catch((err) => console.log('[ERROR] DB connection failed', err));
+
+(async function () {
+
+    //DELETE OLD USERS
+    try{
+        await User.deleteMany({});
+        console.log("Old users deleted");
+    }catch(error){
+        console.log(error);
+    }
+
+    // CREATE 10 FAKE USERS
+
+    const userPromises = Array(10)
+        .fill(null)
+        .map(()=>{
+            const userData = {
+                firstName: faker.name.firstName(),
+                lastName:faker.name.lastName(),
+                userName: faker.internet.userName(),
+                email: faker.internet.email(),
+                birthday: faker.date.past(),
+                password: "01234",
+            };
+
+            const user = new User(userData);
+            return user.save();
+        });
+
+    let usersSeeded;
+
+    try{
+        usersSeeded = await Promise.all(userPromises);
+        console.log("We stored 10 users in the DB");
+    }catch(error){
+        console.log(error);
+    }
+
+    const userIds = usersSeeded.map((user) => user._id);
+
+    //DELETE OLD SKILLS
+     try{
+        await Skill.deleteMany({});
+        console.log("Old skills deleted");
+    }catch(error){
+        console.log(error);
+    }
+
+    const skillPromises = Array(10)
+        .fill(null)
+        .map(()=>{
+            const skillData = {
+                name: faker.commerce.product(),
+                creator: faker.random.arrayElement(userIds)
+            };
+
+            const skill = new Skill(skillData);
+            return skill.save();
+        });
+
+    let skillSeeded;
+
+    try{
+        skillSeeded = await Promise.all(skillPromises);
+        console.log("We stored 10 skills in the DB");
+    }catch(error){
+        console.log(error);
+    }
+
+    const skillIds = skillSeeded.map((skill) => skill._id);
+
+
+
+    //DELETE OLD TASKS
+    try{
+        await Task.deleteMany({});
+        console.log("Old tasks deleted");
+    }catch(error){
+        console.log(error);
+    }
+
+    const possibleStatus = ["pending", "rejected", "finished", "confirmed", "acepted"]
+
+    const taskPromises = Array(10)
+        .fill(null)
+        .map(()=>{
+            const taskData = {
+                skill: faker.random.arrayElement(skillIds),
+                boons: faker.commerce.price(),
+                message: faker.lorem.paragraph(),
+                status: faker.random.arrayElement(possibleStatus),
+                date: faker.date.future(),
+                boonee:faker.random.arrayElement(userIds),
+                booner: faker.random.arrayElement(userIds),
+                rating: Math.floor(Math.random() * (5 - 1 + 1) + 1),
+            };
+
+            const task = new Task(taskData);
+            return task.save();
+        });
+
+    let tasksSeeded;
+
+    try{
+        tasksSeeded = await Promise.all(taskPromises);
+        console.log("We stored 10 tasks in the DB");
+    }catch(error){
+        console.log(error);
+    }  
+
+mongoose.connection.close();
+
+})();
