5c8280d233ba21d19440d6969af50f00b2d014d8
Cindy <Punk.c@hotmail.com>
2021-05-11 14:42:35 +0200

    add skillrouter

--- routes/skillRouter.js
+++ routes/skillRouter.js
@@ -8,4 +8,17 @@ const {
 // /skill
 router.route('/').get(getSkills);
 
+// /skill/id
+// router.route('/').get(function(req, res) {
+//   kennel.find({skill: {$in: ['coding']}},
+//     function(err, result) {
+//       if(err) {
+//         res.send(err);
+//       }else {
+//         res.send(result);
+//       }
+//     })
+// })
+
+
 module.exports = router;
